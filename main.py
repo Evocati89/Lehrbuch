643629b84a4e11f7e0fe485de8c297e5141020bf
Daniel Gabriel Bach <bach@dgb-enterprises.de>
2024-04-10 11:55:32 +0200

    first try

---
+++ main.py
@@ -0,0 +1,20 @@
+#main Datei Lehrbuch Übungen
+#Erste Ausgabe
+print("Hallo Welt!")
+#Erste Ausgabe - Ende
+
+##Übung 2.1.7
+vInch = 2.54
+print(5 * vInch, 
+      20 * vInch, 
+      92.7 * 2.54)
+##Übung 2.1.7 - Ende
+
+"""Mehrzeilige
+Kommentare
+3Anführungszeichen"""
+
+##Übung 2 - etsS
+
+##Übung 2 - Ende
+#test für lens
