0861f6ba17b7901778d074467c21f36ee1fbf916
Daniel Gabriel Bach <bach@dgb-enterprises.de>
2024-04-25 20:26:55 +0200

    Exercise 3.2.1 and 3.2.2

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
+#Exercise 3.2.2
+#defination of variables
+vA = 5
+vB = 3
+
+#calculation
+vC = vA + vB
+
+#output
+print(f"Die Aufgabe: {vA} + {vB}")
+print(f"Das Ergebnis: {vC}")
+print(f"Das Ergebnis ohne Speicherung: {vA + vB}")
+print(f"Eine weitere Berechnung: {(12 - 5 * 2) / 4}")
+#Exersice 3.2.2 - End
+
+#Exercise 3.2.1
+#defination of variables
+vA = 5
+vB = 3
+
+#calculation
+vC = vA + vB
+
+#output
+print("Die Aufgabe:", vA, "+", vB)
+print("Das Ergebnis:", vC)
+#Exersice 3.2.1 - End
+
 #main Datei Lehrbuch Übungen
 #Erste Ausgabe
 print("Hallo Welt!")
